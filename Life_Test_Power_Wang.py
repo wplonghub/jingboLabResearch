0c95f51b824f219ac536aa13f66d68bf2f3ddbd3
Peilong <peilong@localhost.localdomain>
2021-03-30 14:31:31 -0500

    modified Life_Test_Power_Wang.py to remove the 3rd data points

--- Life_Test_Power_Wang.py
+++ Life_Test_Power_Wang.py
@@ -4,20 +4,20 @@ import numpy as np
 import datetime
 
 
-# col = [Date, time, P1(dBm), P2(dBm), P3(dBm), P4(dBm)       , P5(dBm)       , P6(dBm)       , P7(dBm)       , P8(dBm)       , P9(dBm)       , P10(dBm)       , P11(dBm)       , P12(dBm)       , P13(dBm)       , P14(dBm)       , P15(dBm)       , P16(dBm)       , P17(dBm)       , P18(dBm)       , P19(dBm)       , P20(dBm)       , P21(dBm)       , P22(dBm)       , P23(dBm)       , P24(dBm)       , V1(dBm)       , V2(dBm)       , V3(dBm)       , V4(dBm)       , V5(dBm)       , V6(dBm)       , V7(dBm)       , V8(dBm)       , V9(dBm)       , V10(dBm)       , V11(dBm)       , V12(dBm)       , P13(dBm)       .1, V14(dBm)       , V15(dBm)       , V16(dBm)       , V17(dBm)       , V18(dBm)       , V19(dBm)       , V20(dBm)       , V21(dBm)       , V22(dBm)       , V23(dBm)       , V24(dBm)]
-colname = ['Date']
+# # col = [Date, time, P1(dBm), P2(dBm), P3(dBm), P4(dBm)       , P5(dBm)       , P6(dBm)       , P7(dBm)       , P8(dBm)       , P9(dBm)       , P10(dBm)       , P11(dBm)       , P12(dBm)       , P13(dBm)       , P14(dBm)       , P15(dBm)       , P16(dBm)       , P17(dBm)       , P18(dBm)       , P19(dBm)       , P20(dBm)       , P21(dBm)       , P22(dBm)       , P23(dBm)       , P24(dBm)       , V1(dBm)       , V2(dBm)       , V3(dBm)       , V4(dBm)       , V5(dBm)       , V6(dBm)       , V7(dBm)       , V8(dBm)       , V9(dBm)       , V10(dBm)       , V11(dBm)       , V12(dBm)       , P13(dBm)       .1, V14(dBm)       , V15(dBm)       , V16(dBm)       , V17(dBm)       , V18(dBm)       , V19(dBm)       , V20(dBm)       , V21(dBm)       , V22(dBm)       , V23(dBm)       , V24(dBm)]
+# colname = ['Date']
 
-for i in range(1, 25):
-    colname.append('P' + str(i) + '(dBm)')
+# for i in range(1, 25):
+#     colname.append('P' + str(i) + '(dBm)')
 
-# voltage in dBm?
-for i in range(1, 25):
-    colname.append('V' + str(i) + '(dBm)')
+# # voltage in dBm?
+# for i in range(1, 25):
+#     colname.append('V' + str(i) + '(dBm)')
 
-colname.append('End')
+# colname.append('End')
 
-# print (colname)
-# print (len(colname))
+# # print (colname)
+# # print (len(colname))
 
 
 
@@ -42,14 +42,14 @@ print (df2.dtypes)
 # print (df2)
 
 
-# LifeTest_Opowers_20210324.TXT
-df3 = pd.read_table("LifeTest_Opowers_20210324.TXT", index_col=0, parse_dates=True, names=colname2 )
-df3 = df3.loc[df3.index < '2021-02-22 00:00:00']
-df3 = df3.drop(columns=['End'])
-print (df3.dtypes)
-# print (df3)
+# # LifeTest_Opowers_20210324.TXT
+# df3 = pd.read_table("LifeTest_Opowers_20210324.TXT", index_col=0, parse_dates=True, names=colname2 )
+# df3 = df3.loc[df3.index < '2021-02-22 00:00:00']
+# df3 = df3.drop(columns=['End'])
+# print (df3.dtypes)
+# # print (df3)
 
-dfapp = df1.append(df2).append(df3)
+dfapp = df1.append(df2) #.append(df3)
 dfapp = dfapp.groupby(dfapp.index.date).mean()
 dffig = dfapp.plot(marker = ".")
 plt.legend(prop={'size': 3.5}, loc='center left', bbox_to_anchor=(1, 0.5))
