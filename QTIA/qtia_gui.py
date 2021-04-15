77b14487a2c798a622bb3a981c00595bfd89e98a
Peilong <peilong@localhost.localdomain>
2021-04-15 17:01:53 -0500

    added default value for QTIA GUI

--- QTIA/qtia_gui.py
+++ QTIA/qtia_gui.py
@@ -11,6 +11,32 @@ from tkinter import *
 @author: Peilong Wang
 @date: Apr. 7, 2021
 '''
+
+def df_form_reg(l_gui):
+    # gui value -> resiger map
+    l_txt = [0] * 13
+    l_txt[10] = l_gui[0]  
+    l_txt[11] = l_gui[1]  
+    l_txt[4] = l_gui[2]  
+    l_txt[6] = l_gui[3]  
+    l_txt[1] = l_gui[4]  
+    l_txt[0] = l_gui[5]  
+    l_txt[2] = l_gui[6]  
+    l_txt[7] = l_gui[7]  
+    l_txt[12] = l_gui[8]  
+    l_txt[3] = l_gui[9]  
+    l_txt[5] = l_gui[10]  
+    l_txt[9] = l_gui[11]  
+    l_txt[8] = l_gui[12]  
+
+    r0 = l_txt[0] << 4 | l_txt[1]
+    r1 = l_txt[2] << 4 | l_txt[3]
+    r2 = l_txt[4] << 7 | l_txt[5] << 4 | l_txt[6] << 3 | l_txt[7]
+    r3 = l_txt[8] << 7 | l_txt[9] << 6 | l_txt[10] << 4 | l_txt[11] << 3 | l_txt[12]
+
+    return [r0, r1, r2, r3]
+
+
 def write_user_config(Reg_Val, out_filename):
     # write out to configuration file
     f = open(out_filename, 'w')
@@ -28,17 +54,29 @@ duration = 200
 root = Tk()
 root.title("QTIA")
 
-# labels
+# creating labels and shoving labels onto screen
 lb_ch1 = Label(root, text="CH1")
 lb_ch2 = Label(root, text="CH2")
 lb_ch3 = Label(root, text="CH3")
 lb_ch4 = Label(root, text="CH4")
 
+lb_ch4.grid(row=2, column=0)
+lb_ch3.grid(row=3, column=0)
+lb_ch2.grid(row=4, column=0)
+lb_ch1.grid(row=5, column=0)
+
+# creating categories and shoving them onto screen
 lb_cat1 = Label(root, text="Bias enabling", bg='grey')
 lb_cat2 = Label(root, text="Bias selection", bg='grey')
 lb_cat3 = Label(root, text="Bandwidth selection", bg='grey')
 lb_cat4 = Label(root, text="Miscellaneous", bg='grey')
 
+lb_cat1.grid(row=0, column=1, columnspan=4, padx=40, pady=10, sticky='ew')
+lb_cat2.grid(row=0, column=5, columnspan=4, padx=40, pady=10, sticky='ew')
+lb_cat3.grid(row=0, column=9, columnspan=3, padx=30, pady=10, sticky='ew')
+lb_cat4.grid(row=0, column=12, columnspan=2, padx=20, pady=10, sticky='ew')
+
+# creating register labels and them category onto screen
 lb_name = ['PD_TIA<1:0>', 'LA_disBIAS', 'OD_disBIAS', 'LPF_disBIAS', 'IBSel_TIA<1:0>', 'IBSel_LA<2:0>', 'IBSel_OD0<2:0>', 'AmpSel_OD<2:0>', 'BWSel_TIA<2:0>', 'BWSel_LA<2:0>', 'PreEm_OD<2:0>', 'RSSION', 'CH1_CLKOn']
 
 lb_reg1 = Label(root, text=lb_name[0])
@@ -55,18 +93,6 @@ lb_reg11 = Label(root, text=lb_name[10])
 lb_reg12 = Label(root, text=lb_name[11])
 lb_reg13 = Label(root, text=lb_name[12])
 
-
-# shoving labels onto screen
-lb_ch4.grid(row=2, column=0)
-lb_ch3.grid(row=3, column=0)
-lb_ch2.grid(row=4, column=0)
-lb_ch1.grid(row=5, column=0)
-
-lb_cat1.grid(row=0, column=1, columnspan=4, padx=40, pady=10, sticky='ew')
-lb_cat2.grid(row=0, column=5, columnspan=4, padx=40, pady=10, sticky='ew')
-lb_cat3.grid(row=0, column=9, columnspan=3, padx=30, pady=10, sticky='ew')
-lb_cat4.grid(row=0, column=12, columnspan=2, padx=20, pady=10, sticky='ew')
-
 lb_reg1.grid(row=1, column=1)
 lb_reg2.grid(row=1, column=2)
 lb_reg3.grid(row=1, column=3)
@@ -82,31 +108,32 @@ lb_reg12.grid(row=1, column=12)
 lb_reg13.grid(row=1, column=13)
 
 
-
 # add entry to screen
+entry_default_value = [1, 0, 0, 0, 1, 2, 3, 3, 5, 1, 7, 0, 0]
+
 e4 = []
 for j in range(13):
     e4.append( Entry(root, width=5) )
     e4[j].grid(row=2, column=j+1, padx=10, pady=10)
-    e4[j].insert(END, '0')
+    e4[j].insert(END, str(entry_default_value[j]))
 
 e3 = []
 for j in range(13):
     e3.append( Entry(root, width=5) )
     e3[j].grid(row=3, column=j+1, padx=10, pady=10)
-    e3[j].insert(END, '0')
+    e3[j].insert(END, str(entry_default_value[j]))
 
 e2 = []
 for j in range(13):
     e2.append( Entry(root, width=5) )
     e2[j].grid(row=4, column=j+1, padx=10, pady=10)
-    e2[j].insert(END, '0')
+    e2[j].insert(END, str(entry_default_value[j]))
 
 e1 = []
 for j in range(13):
     e1.append( Entry(root, width=5) )
     e1[j].grid(row=5, column=j+1, padx=10, pady=10)
-    e1[j].insert(END, '0')
+    e1[j].insert(END, str(entry_default_value[j]))
 
 
 e_com = Entry(root, width=8, fg='red')
@@ -115,67 +142,45 @@ e_com.insert(END, 'COM3')
 
 i2cAddr = Entry(root, width=8, fg='red')
 i2cAddr.grid(row=3, column=15, padx=10, pady=12)
-i2cAddr.insert(END, '0x20')
+i2cAddr.insert(END, '0x21')
 
-
-
-def df_form_reg(el):
-    # assembly to resiger map
-    l = []
-    l.append( el[10] )
-    l.append( el[11] )
-    l.append( el[4] )
-    l.append( el[6] )
-    l.append( el[1] )
-    l.append( el[0] )
-    l.append( el[2] )
-    l.append( el[7] )
-    l.append( el[12] )
-    l.append( el[3] )
-    l.append( el[5] )
-    l.append( el[9] )
-    l.append( el[8] )
-
-    r0 = l[0] << 4 | l[1]
-    r1 = l[2] << 4 | l[3]
-    r2 = l[4] << 7 | l[5] << 4 | l[6] << 3 | l[7]
-    r3 = l[8] << 7 | l[9] << 6 | l[10] << 4 | l[11] << 3 | l[12]
-
-    return [r0, r1, r2, r3]
 
 def button_run():
+
+    # get entry values from GUI
     Reg_Val = []
 
-    el4 = []
+    e4_value = []
     for i in range(13):
-         el4.append( e4[i].get() )
-    el4 = [int(i) for i in el4]
-    Reg_Val.extend( df_form_reg(el4) )
+         e4_value.append( e4[i].get() )
+    e4_value = [int(i) for i in e4_value]
+    Reg_Val.extend( df_form_reg(e4_value) )
 
-    el3 = []
+    e3_value = []
     for i in range(13):
-         el3.append( e4[i].get() )
-    el3 = [int(i) for i in el3]
-    Reg_Val.extend( df_form_reg(el3) )
+         e3_value.append( e3[i].get() )
+    e3_value = [int(i) for i in e3_value]
+    Reg_Val.extend( df_form_reg(e3_value) )
 
-    el2 = []
+    e2_value = []
     for i in range(13):
-         el2.append( e4[i].get() )
-    el2 = [int(i) for i in el2]
-    Reg_Val.extend( df_form_reg(el2) )
+         e2_value.append( e2[i].get() )
+    e2_value = [int(i) for i in e2_value]
+    Reg_Val.extend( df_form_reg(e2_value) )
 
-    el1 = []
+    e1_value = []
     for i in range(13):
-         el1.append( e4[i].get() )
-    el1 = [int(i) for i in el1]
-    Reg_Val.extend( df_form_reg(el1) )
+         e1_value.append( e1[i].get() )
+    e1_value = [int(i) for i in e1_value]
+    Reg_Val.extend( df_form_reg(e1_value) )
 
     register_filename = "QTIA_gui_register_config.txt"   # need to change by users
 
     COM_Port = e_com.get()
     I2C_Addr = int(i2cAddr.get(), 0)                             # need to change according I2C Addr
 
-    write_user_config(Reg_Val, register_filename)    # comment out this line if you want to use register_filename directly
+    # Write GUI value to txt
+    write_user_config(Reg_Val, register_filename)    
 
     Reg_Addr = []
     Reg_Val = []
@@ -227,17 +232,3 @@ bt_run.grid(row=1, column=15)
 
 root.mainloop()
 
-
-    # l[0] = el[10]
-    # l[1] = el[11]
-    # l[2] = el[4]
-    # l[3] = el[6]
-    # l[4] = el[1]
-    # l[5] = el[0]
-    # l[6] = el[2]
-    # l[7] = el[7]
-    # l[8] = el[12]
-    # l[9] = el[3]
-    # l[10] = el[5]
-    # l[11] = el[9]
-    # l[12] = el[8]
