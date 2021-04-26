47011e364ec9d6aa49d89049d282f7c34098da33
LilyZhang1234 <81389421+LilyZhang1234@users.noreply.github.com>
2021-04-26 17:24:19 -0500

    Add some annotation.

--- GBS20V1/GBS20V1_gui.py
+++ GBS20V1/GBS20V1_gui.py
@@ -11,6 +11,10 @@ from tkinter import *
 @author: Peilong Wang
 @date: Apr. 19, 2021
 '''
+# Updated some reg coordibate and modified PA0_clkinvert and PA1_clkinvert. This version can be closed.
+# May need modify the name of PA0/PA1.
+# @Co-author: Lily Zhang
+# @date: Apr. 26th, 2021
 root = Tk()
 root.title("GBS20V1 GUI")
 
